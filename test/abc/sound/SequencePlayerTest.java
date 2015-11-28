e072e5637331534f74c9c35117ef4ac486cf4ae9
Amber Guo <atguo@mit.edu>
2015-11-27 18:00:30 -0700

    adding testpiece1

--- test/abc/sound/SequencePlayerTest.java
+++ test/abc/sound/SequencePlayerTest.java
@@ -2,19 +2,69 @@ package abc.sound;
 
 import static org.junit.Assert.*;
 
+
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiUnavailableException;
+
 import org.junit.Test;
+
+import abc.player.Main;
 
 public class SequencePlayerTest {
 
     @Test 
-    public void testPiece1(){
+    public void testPiece1() throws MidiUnavailableException, InvalidMidiDataException{
+        SequencePlayer player = new SequencePlayer(140,12);
+        
+        player.addNote(new Pitch('C').toMidiNote(),0 , 12);
+        player.addNote(new Pitch('C').toMidiNote(),12,12);
+        player.addNote(new Pitch('C').toMidiNote(), 24, 9);
+        player.addNote(new Pitch('D').toMidiNote(), 33, 3);
+        player.addNote(new Pitch('E').toMidiNote(), 36, 12);
+        player.addNote(new Pitch('E').toMidiNote(), 48, 9);
+        player.addNote(new Pitch('D').toMidiNote(), 57, 3);
+        player.addNote(new Pitch('E').toMidiNote(), 60, 9);
+        player.addNote(new Pitch('F').toMidiNote(), 69, 3);
+        player.addNote(new Pitch('G').toMidiNote(), 72, 24);
+        player.addNote(new Pitch('C').transpose(Pitch.OCTAVE).toMidiNote(), 96, 4);
+        player.addNote(new Pitch('C').transpose(Pitch.OCTAVE).toMidiNote(), 100, 4);
+        player.addNote(new Pitch('C').transpose(Pitch.OCTAVE).toMidiNote(), 104, 4);
+        player.addNote(new Pitch('G').toMidiNote(), 108, 4);
+        player.addNote(new Pitch('G').toMidiNote(), 112, 4);
+        player.addNote(new Pitch('G').toMidiNote(), 116, 4);
+        player.addNote(new Pitch('E').toMidiNote(), 120, 4);
+        player.addNote(new Pitch('E').toMidiNote(), 124, 4);
+        player.addNote(new Pitch('E').toMidiNote(), 128, 4);
+        player.addNote(new Pitch('C').toMidiNote(), 132, 4);
+        player.addNote(new Pitch('C').toMidiNote(), 136, 4);
+        player.addNote(new Pitch('C').toMidiNote(), 140, 4);
+        player.addNote(new Pitch('G').toMidiNote(), 144, 9);
+        player.addNote(new Pitch('F').toMidiNote(), 153, 3);
+        player.addNote(new Pitch('E').toMidiNote(), 156, 9);
+        player.addNote(new Pitch('D').toMidiNote(), 165, 3);
+        player.addNote(new Pitch('C').toMidiNote(), 168, 24);
+        
+        System.out.println(player);
+        player.play();
+        
+        Main.play("sample_abc/piece1.abc");
+        
+                
+        
         
     }
     
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void testPiece2(){
+        Main.play("sample_abc/piece2.abc");
+        
     }
+    
+    //@Test
+    //public void test() {
+    //    fail("Not yet implemented");
+    //}
     
 
 }
