0469682a571f906030fb7f3474d0f967bed87f60
Amber Guo <atguo@mit.edu>
2015-12-01 20:38:26 -0500

    adding parse to Piece, waiting for Jessica on MakePiece

--- src/abc/sound/Piece.java
+++ src/abc/sound/Piece.java
@@ -1,10 +1,22 @@
 package abc.sound;
+import abc.parser.*;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiUnavailableException;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
 public class Piece {
 	private final List<Voice> voices;
@@ -29,4 +41,27 @@ public class Piece {
 		}
 		return sp;
 	}
+	
+	public static Piece parse(String input){
+	    try{
+	        CharStream stream = new ANTLRInputStream(input);
+	        AbcLexer lexer = new AbcLexer(stream);
+	        TokenStream tokens = new CommonTokenStream(lexer);
+	        AbcParser parser = new AbcParser(tokens);
+	        lexer.reportErrorsAsExceptions();
+	        parser.reportErrorsAsExceptions();
+	        ParseTree tree = parser.root();
+	        
+	        //TODO call it MakePiece?
+	        MakePiece pieceMaker = new MakePiece();
+	        new ParseTreeWalker().walk(pieceMaker, tree);
+	       
+	        
+	        
+	    } catch (RuntimeException e){
+	        throw new IllegalArgumentException("not a valid Piece");
+	    }
+	}
+	
+	
 }
