ecda3f3bc97604d518aa3a8ad3035e8b8bb27472
Jessica <jfang123@mit.edu>
2015-12-02 10:51:39 -0500

    MakeMeasure.java, exitRest

--- src/abc/parser/MakeMeasure.java
+++ src/abc/parser/MakeMeasure.java
@@ -28,6 +28,11 @@ import abc.parser.AbcParser.RootContext;
 import abc.parser.AbcParser.TripletContext;
 import abc.parser.AbcParser.TupletContext;
 import abc.sound.Playable;
+import abc.sound.RatNum;
+import abc.sound.Rest;
+import expressivo.Expression;
+import expressivo.Number;
+import expressivo.Variable;
 
 public class MakeMeasure implements AbcListener {
     private Stack<Playable> stack = new Stack<>();
@@ -41,7 +46,7 @@ public class MakeMeasure implements AbcListener {
     public void exitElement(ElementContext ctx) {
         // TODO Auto-generated method stub
     }
-    
+
     @Override
     public void exitTuplet(TupletContext ctx) {
         // TODO Auto-generated method stub
@@ -54,107 +59,199 @@ public class MakeMeasure implements AbcListener {
 
     @Override
     public void exitRest(RestContext ctx) {
-        // TODO Auto-generated method stub
+        if (ctx.REST() != null) {
+            // figure out what the duration is
+            String duration = ctx.REST().getText().substring(1); //ignore 'z', just want number
+            int numerator;
+            int denominator;
+            if (duration.contains("/")) {
+                if (duration.matches("[0-9]+/")){ //xx/
+                    numerator = Integer.parseInt(duration.substring(0, duration.length()-1));
+                    denominator = 1;
+                } else if (duration.matches("/[0-9]+")){ // /xx
+                    numerator = 1;
+                    denominator = Integer.parseInt(duration.substring(1));
+                } else {
+                    String[] rational = duration.split("/");
+                    numerator = Integer.parseInt(rational[0]);
+                    denominator = Integer.parseInt(rational[1]);
+                }
+          
+            } else if (!duration.contains("/")){ // xx
+                numerator = Integer.parseInt(duration);
+                denominator = 1;
+            }
+            RatNum rational = new RatNum(numerator, denominator);
+            Playable x = new Rest(rational);
+            stack.push(x);
+        } else {}
     }
 
     @Override
     public void exitChord(ChordContext ctx) {
-        // TODO Auto-generated method stub  
+        // TODO Auto-generated method stub
     }
-    
 
     @Override
     public void exitDuplet(DupletContext ctx) {
         // TODO Auto-generated method stub
     }
-    
+
     @Override
     public void exitTriplet(TripletContext ctx) {
-        // TODO Auto-generated method stub   
+        // TODO Auto-generated method stub
     }
-    
+
     @Override
     public void exitQuadruplet(QuadrupletContext ctx) {
-        // TODO Auto-generated method stub   
+        // TODO Auto-generated method stub
     }
-    
-    
-    
-    
-    
-    
+
     @Override
-    public void enterRoot(RootContext ctx) {}
+    public void enterRoot(RootContext ctx) {
+    }
+
     @Override
-    public void exitRoot(RootContext ctx) {}
+    public void exitRoot(RootContext ctx) {
+    }
+
     @Override
-    public void enterEveryRule(ParserRuleContext arg0) {}
+    public void enterEveryRule(ParserRuleContext arg0) {
+    }
+
     @Override
-    public void exitEveryRule(ParserRuleContext arg0) {}
+    public void exitEveryRule(ParserRuleContext arg0) {
+    }
+
     @Override
-    public void visitErrorNode(ErrorNode arg0) {}
+    public void visitErrorNode(ErrorNode arg0) {
+    }
+
     @Override
-    public void visitTerminal(TerminalNode arg0) {}
+    public void visitTerminal(TerminalNode arg0) {
+    }
+
     @Override
-    public void enterHeader(HeaderContext ctx) {}
+    public void enterHeader(HeaderContext ctx) {
+    }
+
     @Override
-    public void exitHeader(HeaderContext ctx) {}
+    public void exitHeader(HeaderContext ctx) {
+    }
+
     @Override
-    public void enterField_number(Field_numberContext ctx) {}
+    public void enterField_number(Field_numberContext ctx) {
+    }
+
     @Override
-    public void exitField_number(Field_numberContext ctx) {}
+    public void exitField_number(Field_numberContext ctx) {
+    }
+
     @Override
-    public void enterField_title(Field_titleContext ctx) {}
+    public void enterField_title(Field_titleContext ctx) {
+    }
+
     @Override
-    public void exitField_title(Field_titleContext ctx) {}
+    public void exitField_title(Field_titleContext ctx) {
+    }
+
     @Override
-    public void enterField_other(Field_otherContext ctx) {}
+    public void enterField_other(Field_otherContext ctx) {
+    }
+
     @Override
-    public void exitField_other(Field_otherContext ctx) {}
+    public void exitField_other(Field_otherContext ctx) {
+    }
+
     @Override
-    public void enterField_composer(Field_composerContext ctx) {}
+    public void enterField_composer(Field_composerContext ctx) {
+    }
+
     @Override
-    public void exitField_composer(Field_composerContext ctx) {}
+    public void exitField_composer(Field_composerContext ctx) {
+    }
+
     @Override
-    public void enterField_length(Field_lengthContext ctx) {}
+    public void enterField_length(Field_lengthContext ctx) {
+    }
+
     @Override
-    public void exitField_length(Field_lengthContext ctx) {}
+    public void exitField_length(Field_lengthContext ctx) {
+    }
+
     @Override
-    public void enterField_meter(Field_meterContext ctx) {}
+    public void enterField_meter(Field_meterContext ctx) {
+    }
+
     @Override
-    public void exitField_meter(Field_meterContext ctx) {}
+    public void exitField_meter(Field_meterContext ctx) {
+    }
+
     @Override
-    public void enterField_tempo(Field_tempoContext ctx) {}
+    public void enterField_tempo(Field_tempoContext ctx) {
+    }
+
     @Override
-    public void exitField_tempo(Field_tempoContext ctx) {}
+    public void exitField_tempo(Field_tempoContext ctx) {
+    }
+
     @Override
-    public void enterField_key(Field_keyContext ctx) {}
+    public void enterField_key(Field_keyContext ctx) {
+    }
+
     @Override
-    public void exitField_key(Field_keyContext ctx) {}
+    public void exitField_key(Field_keyContext ctx) {
+    }
+
     @Override
-    public void enterBody(BodyContext ctx) {}
+    public void enterBody(BodyContext ctx) {
+    }
+
     @Override
-    public void exitBody(BodyContext ctx) {}
+    public void exitBody(BodyContext ctx) {
+    }
+
     @Override
-    public void enterLine(LineContext ctx) {}
+    public void enterLine(LineContext ctx) {
+    }
+
     @Override
-    public void exitLine(LineContext ctx) {}
+    public void exitLine(LineContext ctx) {
+    }
+
     @Override
-    public void enterMeasure(MeasureContext ctx) {}
+    public void enterMeasure(MeasureContext ctx) {
+    }
+
     @Override
-    public void enterElement(ElementContext ctx) {}
+    public void enterElement(ElementContext ctx) {
+    }
+
     @Override
-    public void enterTuplet(TupletContext ctx) {}
+    public void enterTuplet(TupletContext ctx) {
+    }
+
     @Override
-    public void enterNote(NoteContext ctx) {}
+    public void enterNote(NoteContext ctx) {
+    }
+
     @Override
-    public void enterRest(RestContext ctx) {}
+    public void enterRest(RestContext ctx) {
+    }
+
     @Override
-    public void enterChord(ChordContext ctx) {}
+    public void enterChord(ChordContext ctx) {
+    }
+
     @Override
-    public void enterDuplet(DupletContext ctx) {}
+    public void enterDuplet(DupletContext ctx) {
+    }
+
     @Override
-    public void enterTriplet(TripletContext ctx) {}
+    public void enterTriplet(TripletContext ctx) {
+    }
+
     @Override
-    public void enterQuadruplet(QuadrupletContext ctx) {}
+    public void enterQuadruplet(QuadrupletContext ctx) {
+    }
 }
