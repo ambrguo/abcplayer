dee8124ebd212c263483b829d13e3c981ff7a1ef
Casey <jhong47@mit.edu>
2015-12-06 12:56:57 -0500

    working on header listener

---
+++ src/abc/parser/MakeHeader.java
@@ -0,0 +1,213 @@
+package abc.parser;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+import abc.parser.XyzParser.ComposerContext;
+import abc.parser.XyzParser.HeaderContext;
+import abc.parser.XyzParser.IndexContext;
+import abc.parser.XyzParser.KeyContext;
+import abc.parser.XyzParser.LengthContext;
+import abc.parser.XyzParser.MeterContext;
+import abc.parser.XyzParser.NameContext;
+import abc.parser.XyzParser.RootContext;
+import abc.parser.XyzParser.TempoContext;
+import abc.parser.XyzParser.TitleContext;
+import abc.parser.XyzParser.VoiceContext;
+import abc.sound.Header;
+import abc.sound.RatNum;
+
+public class MakeHeader implements XyzListener{
+	private Header header;
+	
+	private int index;
+	private String title;
+	private String composer;
+	private RatNum length;
+	private RatNum meter;
+	private int tempo;
+	private String key;
+
+	@Override
+	public void enterEveryRule(ParserRuleContext arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void exitEveryRule(ParserRuleContext arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visitErrorNode(ErrorNode arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visitTerminal(TerminalNode arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void enterRoot(RootContext ctx) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void exitRoot(RootContext ctx) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void enterHeader(HeaderContext ctx) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void exitHeader(HeaderContext ctx) {
+		try {
+			header = new Header(index, title, key);
+		} catch (Exception e) {
+			throw new RuntimeException();
+		}
+		
+		if (composer != null) {
+			header.setComposer(composer);
+		}
+		
+		if (length != null) {
+			header.setDefaultLength(length);
+		}
+		
+		if (meter != null) {
+			header.setMeter(meter);
+		}
+		
+	}
+
+	@Override
+	public void enterIndex(IndexContext ctx) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void exitIndex(IndexContext ctx) {
+		index = Integer.parseInt(ctx.getText().substring(2).trim());
+	}
+
+	@Override
+	public void enterTitle(TitleContext ctx) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void exitTitle(TitleContext ctx) {
+		title = ctx.getText().substring(2).trim();
+	}
+
+	@Override
+	public void enterComposer(ComposerContext ctx) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void exitComposer(ComposerContext ctx) {
+		composer = ctx.getText().substring(2).trim();
+		
+	}
+
+	@Override
+	public void enterLength(LengthContext ctx) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void exitLength(LengthContext ctx) {
+		String[] len = ctx.getText().split("/");
+		length = new RatNum(Integer.parseInt(len[0]), Integer.parseInt(len[1]));
+		
+	}
+
+	@Override
+	public void enterMeter(MeterContext ctx) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void exitMeter(MeterContext ctx) {
+		String context = ctx.getText().substring(2).trim();
+		if (context.equals("C")) {
+			meter = new RatNum(4, 4);
+		}
+		else if (context.equals("C|")) {
+			meter = new RatNum(2, 2);
+		}
+		else {
+			String[] m = context.split("/");
+			meter = new RatNum(Integer.parseInt(m[0]), Integer.parseInt(m[1]));
+		}
+		
+	}
+
+	@Override
+	public void enterTempo(TempoContext ctx) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void exitTempo(TempoContext ctx) {
+		tempo = Integer.parseInt(ctx.getText().substring(2).trim());
+		
+	}
+
+	@Override
+	public void enterKey(KeyContext ctx) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void exitKey(KeyContext ctx) {
+		key = ctx.getText().substring(2).trim();
+		
+	}
+
+	@Override
+	public void enterVoice(VoiceContext ctx) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void exitVoice(VoiceContext ctx) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void enterName(NameContext ctx) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void exitName(NameContext ctx) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/abc/sound/Header.java
+++ src/abc/sound/Header.java
@@ -1,5 +1,8 @@
 package abc.sound;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Stores data from the header of an abc file 
  */
@@ -14,7 +17,8 @@ public class Header {
     private String composer;
     private RatNum defaultLength; 
     private RatNum meter; 
-    private int tempo;  
+    private int tempo;
+    private List<String> voices;
     
     // Default header field values 
     public static final RatNum DEFAULT_METER = new RatNum(4, 4); 
@@ -38,6 +42,8 @@ public class Header {
         this.setDefaultLength(DEFAULT_LENGTH);
         this.setTempo(DEFAULT_TEMPO);
         this.setComposer(DEFAULT_COMPOSER);
+        
+        this.voices = new ArrayList<>();
     }
     
     public int getIndexNumber() {
@@ -67,5 +73,9 @@ public class Header {
 	
 	public void setComposer(String composer) {
 		this.composer = composer;
+	}
+	
+	public void setVoice(String voice) {
+		voices.add(voice);
 	}
 }
