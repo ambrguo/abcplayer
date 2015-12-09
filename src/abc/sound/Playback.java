510f9166bbcf4743dafde8c2b5d28f71373a89ca
Amber Guo <atguo@mit.edu>
2015-12-09 00:12:37 -0500

    implementing sp in piece

--- src/abc/sound/Playback.java
+++ src/abc/sound/Playback.java
@@ -4,29 +4,33 @@ import java.util.Optional;
 
 public class Playback {
 	private Optional<Pitch> pitch;
-	private Optional<Accidental> accidental;
-	private RatNum duration;
+	private final Accidental accidental;
+	private final RatNum duration;
 	
-	public Playback(Optional<Pitch> pitch, Optional<Accidental> accidental, RatNum duration) {
+	public Playback(Optional<Pitch> pitch, Accidental accidental, RatNum duration) {
 		this.pitch = pitch;
 		this.accidental = accidental;
 		this.duration = duration;
-		checkRep();
+		
 	}
 	
 	public boolean hasPitch() {
 		return pitch.isPresent();
 	}
 	
-	public boolean hasAccidental() {
-		return accidental.isPresent();
+	public Pitch getPitch() {
+	    return pitch.get();
+	}
+	
+	public Accidental getAccidental() {
+		return accidental;
 	}
 	
 	public RatNum getDuration() {
 		return duration;
 	}
 	
-	private void checkRep() {
-		if (pitch.isPresent()) assert accidental.isPresent();
-	}
+//	private void checkRep() {
+//		if (pitch.isPresent()) assert accidental.isPresent();
+//	}
 }
