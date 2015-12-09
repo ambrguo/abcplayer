66980389ae3002f859261ddd854cbc208aac404e
Jessica <jfang123@mit.edu>
2015-12-09 11:19:55 -0500

    PlaybackTest.java, specs

---
+++ test/abc/sound/PlaybackTest.java
@@ -0,0 +1,24 @@
+package abc.sound;
+
+public class PlaybackTest {
+    /**
+     * hasPitch
+     * 1) True
+     * 2) False
+     */
+    /**
+     * getPitch
+     * 1) A-G
+     * 2) a-g
+     * 3) ",", "'"
+     */
+   /**
+    * getAccidental
+    * 1) Sharp 2) Doublesharp 3) Flat 4) Doubleflat 5) Natural 6) None
+    */
+    /**
+     * getDuration
+     * 1) integer
+     * 2) fraction
+     */
+}
